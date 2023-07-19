942d189bd5563e060edb1b77ee02b5e6fa9d4d4b
akashktesla <akashktesla@gmail.com>
2023-07-19 12:58:44 +0530

    :)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(warnings)]
+use gpt_dev_rust::rustpp::*;
+use tch::nn::{Module, OptimizerConfig};
+use tch::{kind, nn, Device, Tensor};
+
+fn main(){
+    let batch_size = 16; // how many independent sequences will we process in parallel?
+    let block_size = 32; // what is the maximum context length for predictions?
+    let max_iters = 5000;
+    let eval_interval = 100;
+    let learning_rate = 1e-3;
+    let eval_iters = 200;
+    let n_embd = 64;
+    let n_head = 4;
+    let n_layer = 4;
+    let dropout = 0.0;
+
+    let f = read_file(&String::from("geng.txt"));
+    
+
+
+
 }
